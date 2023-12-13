4f3a92dc6b6412ed380ac2de473c7fad5ed69e8f
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-12-13 10:42:55 +0000

    2nd betty check

--- mk_tkn.c
+++ mk_tkn.c
@@ -1,6 +1,9 @@
 #include "shell.h"
 /**
  *make_token - tokenisze input string
+ *@args: an array to hold the list of arguments
+ *@userinput: the string that the user typed
+ *Return: nothing
  */
 void make_token(char *args[], char *userinput)
 {
